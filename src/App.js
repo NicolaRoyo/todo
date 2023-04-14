b0bababb867a13c7a7fd7f616f6f67be2cca90d1
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2023-04-13 22:57:09 -0500

    Set up task list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {AiOutlinePlus} from "react-icons/ai";
+import Todo from "./Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to=[#1CB5E0]`
+}
 
 function App() {
+  const [todos, setTodos] = useState(['Work on Next Video', 'Work out!'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Let's Be Productive Today!</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add your tasks love <3.  You've got this" />
+          <button className={style.button}> <AiOutlinePlus size={30} /> </button>
+        </form>
+        <ul>
+          <Todo />
+        </ul>
+
+      </div>
     </div>
   );
 }
